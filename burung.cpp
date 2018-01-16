b02d21dd8f5b98110a33b48b1e0b2c8b490d1311
Dedyset <dedy.smkn2bjb@gmail.com>
2018-01-16 17:50:24 +0700

    cout class burung

--- burung.cpp
+++ burung.cpp
@@ -19,3 +19,16 @@ int main(){
 	burungElang.jenisMakanan = "Ular, Ayam, Kelinci.";
 	burungRajawali.jenisMakanan = "Ular, tikus.";
 	burungCucakrowo.jenisMakanan = "Biji-bijian.";
+	
+	cout <<"Burung Rajawali berwarna " <<(burungRajawali.warnaBulu);
+	cout <<"makanannya " <<(burungRajawali.jenisMakanan);
+	burungRajawali.bersuara("cuittt");
+	
+	cout <<"Burung Cucakrowo berwarna " <<(burungCucakrowo.warnaBulu);
+	cout <<"makanannya " <<(burungCucakrowo.jenisMakanan);
+	burungCucakrowo.bersuara("cokkk");
+}
+
+void burung::bersuara(string bunyi){
+	cout << " Bunyinya "<<bunyi<< " !!"<<endl;
+}
